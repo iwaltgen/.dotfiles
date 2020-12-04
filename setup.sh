acf193e41cdb8a8ddb0a77c2135adfb6b6683feb
iwaltgen <iwaltgen@gmail.com>
2020-12-05 05:48:03 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,4 +11,8 @@ popd
 rm -rf fonts
 popd
 
-apply.sh
+./apply.sh
+
+defaults write -g ApplePressAndHoldEnabled -bool false
+defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
