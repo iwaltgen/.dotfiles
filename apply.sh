db17e74f5631a6da90424df8f62e8f5b17f76402
iwaltgen <iwaltgen@gmail.com>
2021-11-28 15:17:10 +0900

    feat: add os type zshrc

--- apply.sh
+++ apply.sh
@@ -6,10 +6,11 @@ ln -sf ~/.dotfiles/.ideavimrc ~
 ln -sf ~/.dotfiles/.p10k.zsh ~
 ln -sf ~/.dotfiles/.alacritty.yml ~
 
-if [[ $OSTYPE == darwin* ]]; then
-	ln -sf ~/.dotfiles/.zshrc.mac ~/.zshrc.os
-fi
-
+case "$OSTYPE" in
+  darwin*)  ln -sf ~/.dotfiles/.zshrc.mac ~/.zshrc.os ;;
+  linux*)   ln -sf ~/.dotfiles/.zshrc.linux ~/.zshrc.os ;;
+  *)        echo "unknown ostype: $OSTYPE" ;;
+esac
 
 ln -sf ~/.dotfiles/.tmux.conf ~
 ln -sf ~/.dotfiles/.tmux.theme.conf ~
