29494d5c7596dfed812111baad6d9601382d1fd3
iwaltgen <iwaltgen@gmail.com>
2021-11-28 15:52:47 +0900

    fix: ln.zshrc.os

--- apply.sh
+++ apply.sh
@@ -6,12 +6,12 @@ ln -sf ~/.dotfiles/.ideavimrc ~
 ln -sf ~/.dotfiles/.p10k.zsh ~
 ln -sf ~/.dotfiles/.alacritty.yml ~
 
-case "$OSTYPE" in
-  darwin*)  ln -sf ~/.dotfiles/.zshrc.mac ~/.zshrc.os ;;
-  linux*)   ln -sf ~/.dotfiles/.zshrc.linux ~/.zshrc.os ;;
-  *)        echo "unknown ostype: $OSTYPE" ;;
-esac
 touch ~/.zshrc.local
+if [[ $OSTYPE == darwin* ]]; then
+  ln -sf ~/.dotfiles/.zshrc.mac ~/.zshrc.os
+elif [[ $OSTYPE == linux* ]]; then
+  ln -sf ~/.dotfiles/.zshrc.linux ~/.zshrc.os
+fi
 
 ln -sf ~/.dotfiles/.tmux.conf ~
 ln -sf ~/.dotfiles/.tmux.theme.conf ~
