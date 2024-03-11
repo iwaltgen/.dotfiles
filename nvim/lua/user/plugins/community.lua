303bce2922801ed149f62d0787fbf728c345f000
iwaltgen <iwaltgen@gmail.com>
2024-03-11 19:03:43 +0900

    update: astronvim 설정 정리

--- nvim/lua/user/plugins/community.lua
+++ nvim/lua/user/plugins/community.lua
@@ -1,5 +1,4 @@
 return {
-  -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
@@ -7,8 +6,13 @@ return {
   { import = "astrocommunity.colorscheme.github-nvim-theme" },
   { import = "astrocommunity.git.octo-nvim" },
   { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
   { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.proto" },
   { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
