2670cd38e5213f95da44de776b5bef670034a7c3
iwaltgen <iwaltgen@gmail.com>
2024-03-12 16:28:50 +0900

    update: smartcolumn length 120

--- nvim/lua/user/plugins/community.lua
+++ nvim/lua/user/plugins/community.lua
@@ -5,6 +5,13 @@ return {
 
   -- { import = "astrocommunity.bars-and-lines.dropbar-nvim" },
   { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  {
+    "m4xshen/smartcolumn.nvim",
+    opts = {
+      colorcolumn = 120,
+      disabled_filetypes = { "help" },
+    },
+  },
 
   { import = "astrocommunity.colorscheme.github-nvim-theme" },
 
