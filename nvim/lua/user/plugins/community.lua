7ec718795815f93d7fc1fbbee9f568e2f5b178f3
iwaltgen <iwaltgen@gmail.com>
2024-03-15 23:21:20 +0900

    chore: lua 포맷 스타일 변경
    - http://lua-users.org/wiki/LuaStyleGuide Formatting

--- nvim/lua/user/plugins/community.lua
+++ nvim/lua/user/plugins/community.lua
@@ -1,31 +1,31 @@
 return {
-	"AstroNvim/astrocommunity",
-	-- example of importing a plugin, comment out to use it or add your own
-	-- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  "AstroNvim/astrocommunity",
+  -- example of importing a plugin, comment out to use it or add your own
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-	-- { import = "astrocommunity.bars-and-lines.dropbar-nvim" },
-	{ import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
-	{
-		"m4xshen/smartcolumn.nvim",
-		opts = {
-			colorcolumn = 120,
-			disabled_filetypes = { "help" },
-		},
-	},
+  -- { import = "astrocommunity.bars-and-lines.dropbar-nvim" },
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  {
+    "m4xshen/smartcolumn.nvim",
+    opts = {
+      colorcolumn = 120,
+      disabled_filetypes = { "help" },
+    },
+  },
 
-	{ import = "astrocommunity.colorscheme.github-nvim-theme" },
+  { import = "astrocommunity.colorscheme.github-nvim-theme" },
 
-	{ import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
 
-	{ import = "astrocommunity.git.octo-nvim" },
+  { import = "astrocommunity.git.octo-nvim" },
 
-	{ import = "astrocommunity.pack.markdown" },
-	{ import = "astrocommunity.pack.json" },
-	{ import = "astrocommunity.pack.yaml" },
-	{ import = "astrocommunity.pack.toml" },
-	{ import = "astrocommunity.pack.lua" },
-	{ import = "astrocommunity.pack.typescript-all-in-one" },
-	{ import = "astrocommunity.pack.go" },
-	{ import = "astrocommunity.pack.proto" },
-	{ import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.proto" },
+  { import = "astrocommunity.pack.html-css" },
 }
