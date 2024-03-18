d417b2873f060125615c1ef3fa606705e16e91ad
iwaltgen <iwaltgen@gmail.com>
2024-03-18 12:41:56 +0900

    chore: vscode nvim keymap
    - not affected

--- nvim-code/lua/config/keymaps.lua
+++ nvim-code/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap.set
+
+-- Stay in indent mode
+keymap({ "n", "v" }, "<S-Tab>", "<gv", { desc = "Unindent line" })
+keymap({ "n", "v" }, "<Tab>", ">gv", { desc = "Indent line" })
