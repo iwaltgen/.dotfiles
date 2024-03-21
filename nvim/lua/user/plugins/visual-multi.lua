2fa960af9cc72c3754adc2f4b0d330f616ce7dfb
iwaltgen <iwaltgen@gmail.com>
2024-03-22 00:02:07 +0900

    feat: add vim-visual-multi

---
+++ nvim/lua/user/plugins/visual-multi.lua
@@ -0,0 +1,11 @@
+return {
+  "mg979/vim-visual-multi",
+  branch = "master",
+  lazy = false,
+  init = function()
+    vim.g.VM_maps = {
+      ["Find Under"] = "<C-d>",
+      ["Find Subword Under"] = "<C-d>",
+    }
+  end,
+}
